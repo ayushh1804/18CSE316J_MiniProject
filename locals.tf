504084f6ef455a432d2bc181549751e4d95165b3
AYUSH <ayushrajbls@gmail.com>
2024-04-25 14:08:14 +0530

    ecr setup

--- locals.tf
+++ locals.tf
@@ -1,4 +1,6 @@
 locals {
   bucket_name = "ayushtfbucket18"
   table_name  = "statetable"
+
+  ecr_repo_name = "node-app-ecr-repo"
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 terraform {
 
+  backend "s3" {
+    bucket         = "ayushtfbucket18"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "statetable"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
@@ -13,4 +20,10 @@ module "tf-state" {
   source      = "./modules/tf-state"
   bucket_name = local.bucket_name
   table_name  = local.table_name
+}
+
+module "ecrRepo" {
+  source = "./modules/ecr"
+
+  ecr_repo_name = local.ecr_repo_name
 }
